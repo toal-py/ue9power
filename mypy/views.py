92e32a3659044e5ad0279a2b6406ed22a1736b45
toal-py <log@toal.wtf>
2024-03-04 21:15:13 +0100

    modified:   mypy/views.py

--- mypy/views.py
+++ mypy/views.py
@@ -67,10 +67,11 @@ def powerOverview (request):
     month = m[0] if (m) else 'Bisher kein Monat in der DB'
     mPower = m[1] if (m) else 'Bisher keine Daten in der DB'
     
-    '''def extrapolation():
-        currentMonth = (requests.get('https://web.toal.wtf/power/api?mode=m')).json()
-        meanValue = currentMonth['preliminary_power_consumption'] / len(currentMonth['result'])
-        return round(((calendar.monthrange(date.today().year, date.today().month)[1] - len(currentMonth['result'])) * meanValue), 2)'''
+    cm = (requests.get('https://web.toal.wtf/power/api?mode=m')).json()
+
+    def extrapolation(x):        
+        meanValue = x['preliminary_power_consumption'] / len(x['result'])
+        return round(((calendar.monthrange(date.today().year, date.today().month)[1] - len(x['result'])) * meanValue), 2)
     
     contextPower={
         'day':d[0],
@@ -79,7 +80,7 @@ def powerOverview (request):
         'wPower':w[1],
         'month':month,
         'mPower':mPower,
-        #'extrapolationCurrentMonth':extrapolation()
+        'extrapolationCurrentMonth':extrapolation(cm)
     }
     return (HttpResponse(render_to_string('powerOverview.html', context=contextPower)))
 
