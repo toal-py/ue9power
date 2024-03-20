5973eab0c36593308b62a349c6559b62a51a4de2
toal-py <log@toal.wtf>
2024-03-20 21:51:57 +0100

    modified:   mypy/plotView.py modified:   mypy/templates/powerOverview.html

--- mypy/plotView.py
+++ mypy/plotView.py
@@ -2,12 +2,14 @@ import seaborn as sns
 from io import BytesIO
 import base64
 
+
 def renderPlot(data):
     power = [float(elem) for elem in data.values()]
-    plot = sns.barplot(x = list(data.keys()), y = power)
-    '''plot_file = BytesIO()
-    plot.savefig(plot_file, format='png')
-    encoded_file = base64.b64encode(plot_file.getvalue())'''
-    return plot.imshow()
+    plot = sns.barplot(data=data, x = list(data.keys()), y = power)
+    plotFile = BytesIO()
+    plotFigure = plot.get_figure()
+    plotFigure.savefig(plotFile, format='png')
+    encodedFile = base64.b64encode(plotFile.getvalue())
+    return encodedFile
 
 #test_dict = {'22.01.2024': 6.55, '23.01.2024': 4.75, '24.01.2024': 11.92}
