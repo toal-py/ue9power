d3548852c4b56a8344fbd0233960d2e3ba411fd0
toal-py <log@toal.wtf>
2024-03-20 21:09:45 +0100

    modified:   mypy/plotView.py

--- mypy/plotView.py
+++ mypy/plotView.py
@@ -5,9 +5,9 @@ import base64
 def renderPlot(data):
     power = [float(elem) for elem in data.values()]
     plot = sns.barplot(x = list(data.keys()), y = power)
-    plot_file = BytesIO()
+    '''plot_file = BytesIO()
     plot.savefig(plot_file, format='png')
-    encoded_file = base64.b64encode(plot_file.getvalue())
-    return encoded_file
+    encoded_file = base64.b64encode(plot_file.getvalue())'''
+    return plot
 
 #test_dict = {'22.01.2024': 6.55, '23.01.2024': 4.75, '24.01.2024': 11.92}
