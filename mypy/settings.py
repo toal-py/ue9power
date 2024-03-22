88904607aa95f1d2a8fb82239bc3a64ffc0a25c7
toal-py <log@toal.wtf>
2024-03-22 11:25:20 +0100

    modified:   mypy/settings.py

--- mypy/settings.py
+++ mypy/settings.py
@@ -83,14 +83,20 @@ WSGI_APPLICATION = 'mypy.wsgi.application'
 
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:"
+        'ENGINE': os.environ.get('SQL_ENGINE'),
+        'NAME': os.environ.get('SQL_DATABASE'),
+        'USER': os.environ.get('SQL_USER'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD'),
+        'HOST': os.environ.get('SQL_HOST'),
+        'PORT': os.environ.get('SQL_PORT')
     }
 }
 
 CACHES = {
-    'default': {'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'}
-    
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis:6379",
+    }
 }
 
 
