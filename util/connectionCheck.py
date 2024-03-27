ab2c2b7a575a5c7c3b90a1d6da20439be77a872d
toal-py <log@toal.wtf>
2024-03-27 07:36:28 +0100

    modified:   util/connectionCheck.py

--- util/connectionCheck.py
+++ util/connectionCheck.py
@@ -13,7 +13,7 @@ import cClasses
 try:
     response = requests.get('https://web.toal.wtf')
 
-except NewConnectionError as e:
+except ConnectionError as e:
     queryObj = cClasses.errorLog('connection', f'{e}', 400, datetime.now().isoformat(sep=" ", timespec="seconds"))
     query = queryObj.log()
 
