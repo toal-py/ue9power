fe191435d0a248899b9827e535d5de4cab1898dd
toal-py <log@toal.wtf>
2024-04-03 21:01:28 +0200

    modified:   mypy/plotView.py

--- mypy/plotView.py
+++ mypy/plotView.py
@@ -26,21 +26,19 @@ def plotMonthlyShare(data):
     percentageData = {'green': data['green'], 'orange': (sum(data.values()) - data['red']), 'red': sum(data.values())}
     dframe = pd.DataFrame(percentageData, index = [0])
 
-    bar1 = sns.barplot(data = dframe, x = 'red', y = 'red', hue = 'red', orient = 'h', width = 0.4, palette = ['tab:red'], legend = False)
+    bar1 = sns.barplot(data = dframe, x = 'red', y = 'red', hue = 'red', orient = 'h', width = 0.4, palette = ['tab:red'])
     bar1.set(yticklabels=[])
     bar1.set(ylabel=None)
     bar1.tick_params(left=False)
     bar1.set(xlabel=None)
-    for bar in bar1.containers:
-        bar1.bar_label(bar, fmt = f'{data['red']}', label_type = 'center')
     
-    bar2 = sns.barplot(data = dframe, x = 'orange', y = 'red', hue = 'orange', orient = 'h', width = 0.4, palette = ['tab:orange'], legend = False)
+    bar2 = sns.barplot(data = dframe, x = 'orange', y = 'red', hue = 'orange', orient = 'h', width = 0.4, palette = ['tab:orange'])
     bar2.set(yticklabels=[])
     bar2.set(ylabel=None)
     bar2.tick_params(left=False)
-    bar2.set(xlabel=None)       
+    bar2.set(xlabel=None)
     
-    bar3 = sns.barplot(data = dframe, x = 'green', y = 'red', hue = 'green', orient = 'h', width = 0.4, palette = ['tab:green'], legend = False)
+    bar3 = sns.barplot(data = dframe, x = 'green', y = 'red', hue = 'green', orient = 'h', width = 0.4, palette = ['tab:green'])
     bar3.set(yticklabels=[])
     bar3.set(ylabel=None)
     bar3.tick_params(left=False)
