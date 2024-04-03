7c4d13288e7532cbcbcca3f9779873b358c71994
toal-py <log@toal.wtf>
2024-04-03 11:13:26 +0200

    modified:   mypy/templates/plotPage.html

--- mypy/views.py
+++ mypy/views.py
@@ -150,14 +150,16 @@ def plotPage (request):
 
     monthNames = ['dummy', 'Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember']
     plotList = []
+    monthList = []
 
     for elem in range:
         singlePlot = json.loads(apiCall(mode = 'm', dates = elem[0][-6], expand = True))
         shortFormatDays = {elem[0][-10:-8] : elem[1] for elem in singlePlot['result']['days'].items()}
         plotList.append(renderPlot(shortFormatDays))
-        plotList.append(monthNames[int(elem[0][-6])])
+        monthList.append(monthNames[int(elem[0][-6])])
 
     context = {
-        'plots': plotList
+        'plots': plotList,
+        'months': monthList
     }
     return (HttpResponse(render_to_string('plotPage.html', context=context)))
