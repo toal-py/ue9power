71f24d42c22abcf83f523a96ba20f612620c1ee1
toal-py <log@toal.wtf>
2024-04-03 10:46:04 +0200

    modified:   mypy/plotView.py

--- mypy/plotView.py
+++ mypy/plotView.py
@@ -1,17 +1,16 @@
 import seaborn as sns
 from io import BytesIO
 import base64
-from PIL import Image
 
 
 def renderPlot(data):
     colorPal = ['red' if elem > 9.0 else 'orange' if elem >= 5.0 else 'green' for elem in data.values()]
     power = [float(elem) for elem in data.values()]
+    plotFigure = sns.figure()
     plot = sns.barplot(data=data, x = list(data.keys()), y = power, palette=colorPal, saturation=0.75, hue=list(data.keys()), legend=False)    
     for bar in plot.containers:
         plot.bar_label(bar, fontsize=8)
     plotFile = BytesIO()
-    plotFigure = plot.get_figure()
     plotFigure.set_figwidth(10)    
     plotFigure.savefig(plotFile, format='png')
     plotFile.seek(0)
