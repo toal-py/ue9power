0e6f74de16a9bfcd98db89a6d8d835226fbf07fc
toal-py <log@toal.wtf>
2024-04-03 09:39:45 +0200

    modified:   mypy/plotView.py

--- mypy/plotView.py
+++ mypy/plotView.py
@@ -12,11 +12,11 @@ def renderPlot(data):
         plot.bar_label(bar, fontsize=8)
     plotFile = BytesIO()
     plotFigure = plot.get_figure()
-    plotFigure.set_figwidth(10)
+    plotFigure.set_figwidth(10)    
     plotFigure.savefig(plotFile, format='png')
     plotFile.seek(0)
-    img = Image.open(plotFile)
-    encodedFile = base64.b64encode(img.tobytes())
+    with open(plotFile, 'rb') as f:
+        encodedFile = base64.b64encode(f.read())
     plotFile.close()
     return encodedFile.decode('utf-8')
     
