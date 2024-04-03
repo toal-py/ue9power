7ef4f615daffb07d38259c23d666c9f33c5e4662
toal-py <log@toal.wtf>
2024-04-03 15:44:23 +0200

    s

--- mypy/plotView.py
+++ mypy/plotView.py
@@ -22,12 +22,12 @@ def plotMonthlyOverview(data):
 
 def plotMonthlyShare(data):
     colorPal = {'green': '#74c69d','yellow': '#f48c06','red': '#d00000'}
-    plotFigure = plt.figure()
-    #dframe = pd.DataFrame(data, index=[0])
-    sns.barplot(data = pd.DataFrame({'red': [sum(data.values())]}), x = 100, hue = 'red')
-    sns.barplot(data = pd.DataFrame({'yellow': [sum(data.values()) - data['red']]}), x = 100, hue = 'yellow')
-    sns.barplot(data = pd.DataFrame({'green': [data['green']]}), x = 100, hue = 'green')
-    plotFile = BytesIO()  
+    #plotFigure = plt.figure()
+    plot = sns.barplot(data = pd.DataFrame({'red': [sum(data.values())]}), x = 100, hue = 'red', palette = colorPal)
+    #sns.barplot(data = pd.DataFrame({'yellow': [sum(data.values()) - data['red']]}), x = 100, hue = 'yellow', palette = colorPal)
+    #sns.barplot(data = pd.DataFrame({'green': [data['green']]}), x = 100, hue = 'green', palette = colorPal)
+    plotFile = BytesIO()
+    plotFigure = plot.get_figure()  
     plotFigure.savefig(plotFile, format='png')
     plotFile.seek(0)
     encodedFile = base64.b64encode(plotFile.getbuffer())
