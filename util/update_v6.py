017c2f8fc963a04656ce3e5b6cd33f6274e64eef
toal-py <log@toal.wtf>
2024-08-17 21:39:08 +0200

    added console feedback to update_v6

--- util/update_v6.py
+++ util/update_v6.py
@@ -17,15 +17,18 @@ def getCurrentIPv6():
 def updateIPv6(ipv6,token):
     if ipv6 is not None:
         r = requests.get(f'https://dynv6.com/api/update?hostname=web.toal.wtf&token={token}&ipv6={ipv6}')
+        print ('Success!')
         if r.status_code != 200:
             errMessage = f'Could\'nt update IPv6 address. \n HTTP Error Code: {r.status_code}. \n Error Message: {r.text}.'
             errSubject = 'Error: Could\'nt update IPv6 address'
             err = cClasses.mailing(errMessage,errSubject)
             err.sendMail()
+            print (f'That didn\'t work. \n HTTP Error Code: {r.status_code}. \n Error Message: {r.text}.')
     else:
         errMessage = 'Could\'nt update IPv6 address. No connection to online service.'
         errSubject = 'Error: Could\'nt update IPv6 address'    
         err = cClasses.mailing(errMessage,errSubject)
         err.sendMail()
+        print (f'That didn\'t work. {errMessage}')
 
 updateIPv6(getCurrentIPv6(),os.environ.get('DYNV6_TOKEN'))
