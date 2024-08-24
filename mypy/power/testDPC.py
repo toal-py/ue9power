eb082e4bbe283305bf568b94b8905e730a94ff8f
toal-py <log@toal.wtf>
2024-08-24 21:51:46 +0200

    modified:   mypy/power/testDPC.py

--- mypy/power/testDPC.py
+++ mypy/power/testDPC.py
@@ -19,7 +19,7 @@ def checkTimestampValidity (databaseResultList):
         dictElem = json.loads(elem['val'])
         print (type(dictElem))
         print (dictElem['Time'])
-        datetimeObject = datetime.strptime(dictElem['Time'], '%Y-%m-%d')
+        datetimeObject = datetime.strptime(dictElem['Time'], '%Y-%m-%d %H:%M:%S')
         print (datetimeObject)
         print (datetime.now() - datetimeObject)
 
