368617cdd47e997cb58888630ae381bcc3726839
toal-py <log@toal.wtf>
2024-11-20 13:54:33 +0100

    final bug fix yearlyPowerConsumption

--- mypy/power/yearlyPowerConsumption.py
+++ mypy/power/yearlyPowerConsumption.py
@@ -1,11 +1,7 @@
 import psycopg
 from datetime import date
-#import dotenv
 import os
 import cClasses
-
-
-#dotenv.read_dotenv('/env/.env')
 
 conn = psycopg.connect(os.environ.get('POSTGRES_CONNECT_DB_POWER'))
 cur = conn.cursor()
@@ -21,8 +17,6 @@ def getYearlyPowerConsumption(year:int, data):
         if int(elem[0][-4:]) == year:
             listOfMonths.append(elem[1])
 
-    print (listOfMonths)
-
     if len(listOfMonths) != 12:
         errMessage=f'Couldn\'t write yearly power consumption to database due to insufficient amount of values. Got {len(listOfMonths)}, needed 12.'
         errSubject='Error when writing yearly power consumption'
@@ -37,7 +31,7 @@ def getYearlyPowerConsumption(year:int, data):
 
 #fakeData = [('01.2024', 210.55),('02.2024', 199.6),('03.2024', 194.06),('04.2024', 188.97),('05.2024', 195.17),('06.2024', 204.27),('07.2024', 183.9),('08.2024', 203.96),('09.2024', 169.73),('10.2024', 193.86),('11.2024', 193.86),('12.2024', 193.86)]
 
-getYearlyPowerConsumption(2024, allData)
+getYearlyPowerConsumption(lastYear, allData)
 
 cur.close()
 conn.close()
