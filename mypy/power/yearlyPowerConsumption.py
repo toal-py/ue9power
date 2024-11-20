8c4ab3412852cd38b418058d78a23363eb7ad9d7
toal-py <log@toal.wtf>
2024-11-20 13:50:29 +0100

    bugfixing len(listOfMonths)

--- mypy/power/yearlyPowerConsumption.py
+++ mypy/power/yearlyPowerConsumption.py
@@ -1,10 +1,11 @@
 import psycopg
 from datetime import date
-import dotenv
+#import dotenv
 import os
 import cClasses
 
-dotenv.read_dotenv('/env/.env')
+
+#dotenv.read_dotenv('/env/.env')
 
 conn = psycopg.connect(os.environ.get('POSTGRES_CONNECT_DB_POWER'))
 cur = conn.cursor()
@@ -19,6 +20,9 @@ def getYearlyPowerConsumption(year:int, data):
     for elem in data:
         if int(elem[0][-4:]) == year:
             listOfMonths.append(elem[1])
+
+    print(os.environ.get('POSTGRES_CONNECT_DB_POWER'))
+    print (listOfMonths)
 
     if len(listOfMonths) != 12:
         errMessage=f'Couldn\'t write yearly power consumption to database due to insufficient amount of values. Got {len(listOfMonths)}, needed 12.'
