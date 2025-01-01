7d9bf4e6c3d953e786db03227ecb3575c6158c71
toal-py <log@toal.wtf>
2025-01-01 20:04:16 +0100

    bug fix monthly power consumption. did not properly consider the years when 'collecting' the days of a month.

--- mypy/power/monthlyPowerConsumption.py
+++ mypy/power/monthlyPowerConsumption.py
@@ -10,7 +10,9 @@ dotenv.read_dotenv('/var/www/python-project/ue9power/.env')
 #dotenv.read_dotenv()
 conn = psycopg.connect(os.environ.get('POSTGRES_CONNECT_DB_POWER'))
 cur = conn.cursor()
-cur.execute(f"SELECT date, power_consumption FROM daily_power WHERE REGEXP_LIKE(date,'^(3[01]|[12][0-9]|0[1-9]).{(str(((date.today().replace(day=1))-timedelta(1)).month)).zfill(2)}.(202[0-9]|203[0-9])$');")
+regexMonth = (str(((date.today().replace(day=1))-timedelta(1)).month)).zfill(2)
+regexYear = str(((date.today().replace(day=1))-timedelta(1)).year)
+cur.execute(f"SELECT date, power_consumption FROM daily_power WHERE REGEXP_LIKE(date,'^(3[01]|[12][0-9]|0[1-9]).{regexMonth}.{regexYear}$');")
 allData = cur.fetchall()
 #get number of days of the last month like this: (calendar.MONDAY, 31)
 #calendar.monthrange((date.today().year),(((date.today().replace(day=1))-timedelta(1)).month))
@@ -20,10 +22,10 @@ def getMonthlyPowerConsumption(data,month_days=calendar.monthrange((date.today()
         mpc=0.0
         for elem in data:
             mpc+=elem[1]
-        cur.execute(f"INSERT INTO monthly_power (month_year, month_short_name, power_consumption) VALUES ('{(str(((date.today().replace(day=1))-timedelta(1)).month)).zfill(2)}.{(date.today().year)}', '{(calendar.month_abbr[(((date.today().replace(day=1))-timedelta(1)).month)]).upper()}', {mpc});")
+        cur.execute(f"INSERT INTO monthly_power (month_year, month_short_name, power_consumption) VALUES ('{(str(((date.today().replace(day=1))-timedelta(1)).month)).zfill(2)}.{((date.today().replace(day=1))-timedelta(1)).year}', '{(calendar.month_abbr[(((date.today().replace(day=1))-timedelta(1)).month)]).upper()}', {mpc});")
         conn.commit() 
     else:
-        errMessage=f'Couldn\'t write monthly power consumption to database due to insufficient amount of values. Got {len(data)}, needed {month_days}.'
+        errMessage=f'Couldn\'t write monthly power consumption to database due to invalid amount of values. Got {len(data)}, needed {month_days}.'
         errSubject='Error when writing monthly power consumption'
         err=cClasses.mailing(errMessage,errSubject)
         err.sendMail()
