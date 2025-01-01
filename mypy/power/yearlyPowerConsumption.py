af8d12ac55a7ce92792b0892fd4d101807b65545
toal-py <log@toal.wtf>
2025-01-01 20:09:30 +0100

    forgot to close brackets on yearlyPowerConsumption ...

--- mypy/power/yearlyPowerConsumption.py
+++ mypy/power/yearlyPowerConsumption.py
@@ -26,7 +26,7 @@ def getYearlyPowerConsumption(year:int, data):
         yearlyPowerConsumption = 0.0
         for elem in listOfMonths:
             yearlyPowerConsumption += elem
-        cur.execute(f"INSERT INTO yearly_power (year, power_consumption) VALUES ('{year}', {round(yearlyPowerConsumption, 2)};")
+        cur.execute(f"INSERT INTO yearly_power (year, power_consumption) VALUES ('{year}', {round(yearlyPowerConsumption, 2)});")
         conn.commit()
 
 #fakeData = [('01.2024', 210.55),('02.2024', 199.6),('03.2024', 194.06),('04.2024', 188.97),('05.2024', 195.17),('06.2024', 204.27),('07.2024', 183.9),('08.2024', 203.96),('09.2024', 169.73),('10.2024', 193.86),('11.2024', 193.86),('12.2024', 193.86)]
